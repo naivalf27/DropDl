41d36ab020ea2e7b86a0660b3a1956141ad52b1a
Flavian Mary <Naivalf27@macbook-pro-de-flavian.home>
2017-02-28 13:53:34 +0100

    fix add request

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ app.post('/add/request', function (request, response) {
     }
     // SQL Query > Insert Request
     console.log('request name : '+message['name'].toString('hex'));
+    
     const query1 = client.query('INSERT INTO requests (TYPE_ID,NAME,COMMENT) VALUES ('+message['type_id']+',\''+message['name'].toString('hex')+'\',\''+message['comment'].toString('hex')+'\') RETURNING ID;', [], function(err,result) {
       if(err) {
         console.log('Error insert request : '+err);
